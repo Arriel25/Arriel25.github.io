18a45ef5d8cb4709c19c01756349fdbbdbec9f6c
Arriel25 <liyinuo907@gmail.com>
2024-12-02 14:27:51 -0500

    Update home.js

--- home.js
+++ home.js
@@ -90,12 +90,3 @@ async function getWeather() {
             </div>`;
     }
 }
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
